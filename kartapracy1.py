afbd8aede2fe5c78815bcf00163796852c69c40c
1B02OB <114915078+Borsuk2115@users.noreply.github.com>
2022-10-05 12:12:20 +0000

    zadanie 6 zrobione

--- kartapracy1.py
+++ kartapracy1.py
@@ -9,3 +9,18 @@ print(a**2 + b**2)
 a = int(input())
 b = int(input())
 print((a+b)**2)
+
+# Zad 3
+
+a = int(input())
+b = int(input())
+print((a-b)**3)
+#Zad 4
+
+#a, b, c = map(int, input().split())
+#print(a+b+c)
+
+#Zad 6
+
+brutto =float(input())
+print(brutto / 1.23)
