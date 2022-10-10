bc5a8244cf997c208e45614a42a976ed3a6ae1b2
1B02OB <114915078+Borsuk2115@users.noreply.github.com>
2022-10-10 10:19:38 +0000

    Coś tam zrobione

---
+++ kartapracy2.py
@@ -0,0 +1,21 @@
+#Zad 1
+n = int(input())
+if n % 3 ==0:
+  print("TAK")
+else:
+  print("NIE")
+
+
+#Zad 2
+n = int(input())
+if n>= 100 and n <1000 and n % 17 == 0:
+  print("Tak")
+else:
+  print("Nie")
+
+#Zad 3
+n = int(input())
+if wiek >= 18
+  print("Tak jesteś pełnoletni")
+else:
+print("Nie jesteś LOOOL")
