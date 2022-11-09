406436ba03b57c7bfdb95e12dbedded6be35caa1
1B02OB <114915078+Borsuk2115@users.noreply.github.com>
2022-11-09 13:15:13 +0000

    powtórka

--- kartapracy3b.py
+++ kartapracy3b.py
@@ -1,45 +1,45 @@
-# # # #ZAD 1
-# # # for i in range(1, 31):
-# # #   print(i)
+# # # # #ZAD 1
+# # # # for i in range(1, 31):
+# # # #   print(i)
 
-# # #zad 2
-# # for i in range(1,10,2):
-# #   print(i**2, end=" ")
+# # # #zad 2
+# # # for i in range(1,10,2):
+# # #   print(i**2, end=" ")
 
-# # #zad 3
-# # for i in range(1000,9999):
-# #   if i%379 ==0:
-# #     print(i)
+# # # #zad 3
+# # # for i in range(1000,9999):
+# # #   if i%379 ==0:
+# # #     print(i)
 
-# #zad 4
-# # for i in range(100,999):
-# #   if i%5 ==0 and i%6 ==0 and i%11 ==0:
-# #     print(i)
+# # #zad 4
+# # # for i in range(100,999):
+# # #   if i%5 ==0 and i%6 ==0 and i%11 ==0:
+# # #     print(i)
 
-# #zad 5
-# n= int(input("ile liczb"))
-# suma=0
-# for i in range(1, n+1):
-#   k= int(input())
-#   suma += k
-# print(suma)
+# # #zad 5
+# # n= int(input("ile liczb"))
+# # suma=0
+# # for i in range(1, n+1):
+# #   k= int(input())
+# #   suma += k
+# # print(suma)
 
-# #zad 6
-# k= int(input())
-# a = 0
-# for i in range(0,2 * k, 2):
-#   a += i
-# print(a)
+#zad 6
+k= int(input())
+a = 0
+for i in range(0,2 * k, 2):
+  a += i
+print(a)
 
-#zad 8
-W0= int(input("Podaj liczbe początkowych inwestycji"))
-L=int(input("Podaj lata inwestycji"))
-Wk = 0
-suma = Wk
-for i in range(0, L * 12):
-  Wk = suma * 0,06 * (1/12)
-  suma = suma+Wk
-  print("wartość końcowa inwestycji")
-  print(suma)
+# #zad 8
+# W0= int(input("Podaj liczbe początkowych inwestycji"))
+# L=int(input("Podaj lata inwestycji"))
+# Wk = 0
+# suma = Wk
+# for i in range(0, L * 12):
+#   Wk = suma * 0,06 * (1/12)
+#   suma = suma+Wk
+#   print("wartość końcowa inwestycji")
+#   print(suma)
 
 #zad 9
