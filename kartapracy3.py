c16ff23119af5453c03ecfc8bbe4a5a7f7684071
1B02OB <114915078+Borsuk2115@users.noreply.github.com>
2022-11-28 11:05:23 +0000

    Zadania z liczbami pierwszymi

--- kartapracy3.py
+++ kartapracy3.py
@@ -1,44 +1,10 @@
-   # # # # # #zad 1
-# # # # # n = int(input())
+# #zad 1
+# n = int(input())
 
-# # # # # for i in rage(n):
-# # # # #     print(i**3 + 3, end= " ")
+# for i in rage(n):
+#     print(i**3 + 3, end= " ")
 
-# # # # #ZAD 2
+#ZAD 2
 
-# # # # for i in range(105,10000,15):
-# # # #   print(i,end=" ")
-
-# # # #zad 3
-# # # n = int(input())
-# # # for i in range(1, n+1):
-# # #   if n% i  == 0:
-# # #     print(i, end=" ")
-
-# # # #zad 4
-# # # s = 0
-# # # for i in range(10, 100):
-# # #     s = s + i
-# # # print(s)
-
-# # #zad 5
-# # n = int(input("w ile gramy?"))
-# # suma = n * (n+1) // 2                           
-
-# # for i in range(n-1):
-# #   a = int(input())
-# #   sume = suma - a
-
-
-# #zad napisz pętlę sumującą liczby dwucyfrowe parzyste
-# suma = 0
-# for i in range(10,100, 2):
-#   suma = suma + i
-# print(suma)
-
-
-
-# # print(suma)
-
-#zad 6
-
+for i in range(105,10000,15):
+  print(i,end=" ")
