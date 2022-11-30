5e8b637ebc483da70287594fcf1f3b9ec738244a
1B02OB <114915078+Borsuk2115@users.noreply.github.com>
2022-11-30 13:15:28 +0000

    Zadania z NWW z odejmowaniem

--- NWWiNWD.py
+++ NWWiNWD.py
@@ -1,13 +1,35 @@
+# # a= int(input())
+# # b= int(input())
+# # while a != b:
+# #   if a > b : a = a - b
+# #   elif b > a : b- a
+# # print(a)
+
+
 # a= int(input())
-# b= int(input())
-# while a != b:
-#   if a > b : a = a - b
-#   elif b > a : b- a
-# print(a)
+# b= int(inpput())
+
+# while b > 0:
 
 
-a= int(input())
-b= int(inpput())
 
+  #zadanie nww
+
+a = int(input())
+b = int(input())
+iloczyn = a * b
+while a != b:
+  if a > b : a = a - b
+  elif b > a : b - a
+NWD = a
+
+print(iloczyn // NWD)
+
+#to samo tylko modulo
+a = int(input())
+b = int(input())
+iloczyn = a * b
 while b > 0:
+  a, b = b, a%b
+  print(a)
   
