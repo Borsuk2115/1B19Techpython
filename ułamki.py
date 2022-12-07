f9a36b04403a361610c4bf3f74678c5fb3dd95df
1B02OB <114915078+Borsuk2115@users.noreply.github.com>
2022-12-07 13:10:55 +0000

    Początkowe zadania z ułamkami

---
+++ ułamki.py
@@ -0,0 +1,25 @@
+#zad mnożenie 
+
+# a= int(input("liczik z pierwszej lizcby"))
+# b= int(input("mianownik pierwszej liczby"))
+# c= int(input("licznik drugiej liczby"))
+# d= int(input("mianownik drugiej liczby"))
+# licznik= a*c
+# mianownik= b*d
+# print(licznik)
+# print(mianownik)
+
+#zadodawanie
+# a= int(input("liczik z pierwszej lizcby"))
+# b= int(input("mianownik pierwszej liczby"))
+# c= int(input("licznik drugiej liczby"))
+# d= int(input("mianownik drugiej liczby"))
+# x, y = b, d
+# iloczyn = x *y
+# while y > 0:
+#   x, y = y, x % y
+# nww = iloczyn // x
+# print(f"{a}/{b} + {c}/{d} = ?/{nww} +/{nww} = ?/{nww}")
+
+
+  
