227d331d220792cac0ac9a046ad65e087fa33ab1
1B02OB <114915078+Borsuk2115@users.noreply.github.com>
2023-03-01 13:14:03 +0000

    Napisy zaczęte

---
+++ nap1.py
@@ -0,0 +1,17 @@
+# s = input()
+# L = input()
+# print (s, L)
+# L.sort()
+# s= "".join(L)
+# print(s, L)
+
+
+s = input()
+L = input()
+if s==L:
+  L.reverse
+s="".join(L)
+if L==s:
+  print("Tak")
+  if else:
+  print("Nie")
